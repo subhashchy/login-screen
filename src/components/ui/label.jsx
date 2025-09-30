65496ba39ac7c96f5962ae3da4a1246f9b14e117
Subhash Choudhary <subhash.chandra16@gmail.com>
2025-09-30 19:39:38 +0530

    Add src/components/ui/label.jsx

---
+++ src/components/ui/label.jsx
@@ -0,0 +1,15 @@
+import * as React from "react"
+import { cn } from "@/lib/utils"
+
+const Label = React.forwardRef(({ className, ...props }, ref) => {
+ return (
+ <label
+ ref={ref}
+ className={cn("block text-sm font-medium text-gray-700 mb-1", className)}
+ {...props}
+ />
+ )
+})
+Label.displayName = "Label"
+
+export { Label }
